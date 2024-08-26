edfee5586a2d7bb5267ee5f8557d35737ecb7c9e
TN4HPEL3 <ctnahpele@icloud.com>
2024-08-26 18:07:53 +0200

    Using Types ': number' on function declaration

---
+++ course-starting-project/app.ts
@@ -0,0 +1,10 @@
+// adding ': number' on function declaration you can easly say which type of data you need (in this example I need numbers on both parameters)
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
